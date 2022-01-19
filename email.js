ea74c0a7deba785d920b8f8e2d94768d59c09ffc
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-19 13:44:01 +0000

    added regex

--- email.js
+++ email.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
 const txtFile = fs.readFileSync("test.txt", {encoding: 'utf8'});
 
-let counter = 0;
-
+const regex = /@softwire.com\b/g
  
-for(let i = 0; i <= txtFile.length; i++) {
-  if (txtFile.substring(i, i+13) === "@softwire.com") { 
-      counter += 1
-    }
-}
+let found = txtFile.match(regex);
  
-console.log(counter);
+console.log(found.length);
