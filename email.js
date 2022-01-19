74ae3a6afc163b4b035efec53620f869d05ee03a
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-19 17:51:58 +0000

    Print out the 10 most common domains in order.

--- email.js
+++ email.js
@@ -1,16 +1,14 @@
 const fs = require("fs");
 const txtFile = fs.readFileSync("test.txt", {encoding: 'utf8'});
 
-const regex = /@softwire.com\b/g
-const domainRegex = /@\S*/g
+const regex = /@softwire.com\b/g //regex for softwire.com domain
+const domainRegex = /@\S*/g //regex for all domains
  
-let found = txtFile.match(domainRegex);
+let found = txtFile.match(domainRegex); //creates array of all domains in text file
 
-const dictionary = {
+const dictionary = {}; //empty dictionary oject
 
-}
-//array1.forEach(element => console.log(element));
-
+//loops through found array to idenity unique domains, add to dictionary, and keep track of count
 for (let i = 0; i < found.length; i++) {
   if (dictionary[found[i]] === undefined) {
     dictionary[found[i]] = 1;
@@ -18,7 +16,16 @@ for (let i = 0; i < found.length; i++) {
     dictionary[found[i]] = dictionary[found[i]] + 1;
   }
 }
- 
-console.log(Object.keys(dictionary).length);
-console.log(dictionary);
-console.log(dictionary['@yahoo.com'] + 1)
+
+//creating 2d array from domain data in dictionary
+const domainArray = []; 
+for (var domain in dictionary) {
+    domainArray.push([domain, dictionary[domain]]);
+}
+
+//logs top x domains to the console
+const logTopDomains = (array, n) => {
+  array.sort((a, b) => a[1] - b[1]).reverse();
+  return array.slice(0, n);
+}
+console.log(logTopDomains(domainArray, 10));
