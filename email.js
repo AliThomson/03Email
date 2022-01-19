776b565376fd403a558594bd5860b73b444b54bc
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-19 15:12:51 +0000

    Part 3: added dictionary to find unique domains and add total of each

--- email.js
+++ email.js
@@ -2,7 +2,23 @@ const fs = require("fs");
 const txtFile = fs.readFileSync("test.txt", {encoding: 'utf8'});
 
 const regex = /@softwire.com\b/g
+const domainRegex = /@\S*/g
  
-let found = txtFile.match(regex);
+let found = txtFile.match(domainRegex);
+
+const dictionary = {
+
+}
+//array1.forEach(element => console.log(element));
+
+for (let i = 0; i < found.length; i++) {
+  if (dictionary[found[i]] === undefined) {
+    dictionary[found[i]] = 1;
+  } else {
+    dictionary[found[i]] = dictionary[found[i]] + 1;
+  }
+}
  
-console.log(found.length);
+console.log(Object.keys(dictionary).length);
+console.log(dictionary);
+console.log(dictionary['@yahoo.com'] + 1)
